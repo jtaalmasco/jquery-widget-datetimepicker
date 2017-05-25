cb8357e0cc3ce406b2ca29c14b343610a0a537a0
Jerred Almasco <jtaalmasco@gmail.com>
2017-05-25 13:15:01 +0800

    create widget

--- datetimepicker.js
+++ datetimepicker.js
@@ -1,1 +1,17 @@
 //customize datetimepicker
+$(function(){
+    $.widget("custom.datetimepicker1",{
+        options:{
+
+        },
+        _create: function(){
+            this.element.addClass("custom-datetimepicker");
+            $('<div class="datetimepicker-main-container"></div>').appendTo(this.element);
+        },
+        _init: function(){
+            
+        }
+    })
+
+    //helpers
+})
